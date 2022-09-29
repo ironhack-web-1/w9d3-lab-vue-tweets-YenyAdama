3e36d03222a77f6af5f9552ce40620c762e42ade
YenyAdama <108185798+YenyAdama@users.noreply.github.com>
2022-09-29 17:50:19 +0200

    added

---
+++ src/components/Message.vue
@@ -0,0 +1,34 @@
+<template>
+  <p className="message">{{ message }}</p>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+
+
+
+<template>
+  <div className="actions">
+    <i class="far fa-comment"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="far fa-heart"></i>
+    <i class="fas fa-share"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Actions",
+};
+</script>
+
